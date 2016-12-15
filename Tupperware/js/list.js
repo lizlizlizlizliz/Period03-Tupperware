762dfd4f1bc9ccf613f77df976c213329f3f661a
lizlizlizlizliz <472750237@qq.com>
2016-12-15 21:22:05 +0800

    null

--- Tupperware/js/list.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-			$(".main-secondlist").hover(function(){
-				$(this).children("ul").stop(true,true).slideDown("1000");
-			},function(){
-				$(this).children("ul").stop(true,false).slideUp("1000");
-			})
-})
